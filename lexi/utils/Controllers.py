a0dd573339accf93dd351d399a9fe367c4ac436a
Dzheremi-belpois <chichvarov1315@gmail.com>
2025-01-10 18:29:27 +0300

    feat: new class for sidebar rows
    - refactor: optimized `Controllers`

--- lexi/utils/Controllers.py
+++ lexi/utils/Controllers.py
@@ -1,3 +1,14 @@
+"""Module with controller classes for Collections, single collection and Lexicons
+
+   Supported controllers: ::
+
+        CollectionsController(collections_dir: str) -> New controller for all\
+ collections
+        CollectionController(collection_path: str) -> New controller for single\
+ collection
+        LexiconController(lexicon_path: str) -> New controller for lexicon
+"""
+
 import os
 import random
 import shutil
@@ -24,20 +35,27 @@ class CollectionsController:
     def __init__(self, collections_dir: str) -> None:
         shared.collection_controller = self
         self.collections_dir: str = collections_dir
-        self.collections_file: TextIO = open(
-            collections_dir + "/collections.yaml", "r+", encoding="utf-8"
+        self.collections_file_path: str = os.path.join(
+            collections_dir, "collections.yaml"
         )
-        self.collections: list = yaml.safe_load(self.collections_file)
+
+        if not os.path.exists(self.collections_file_path):
+            with open(self.collections_file_path, "w", encoding="utf-8") as file:
+                file.write("[]")
+
+        self.collections_file: TextIO = open(
+            self.collections_file_path, "r+", encoding="utf-8"
+        )
+        self._collections: list = yaml.safe_load(self.collections_file) or []
 
     def save_collections(self, *_args) -> None:
         """Saves instace collections to the file"""
         self.collections_file.seek(0)
         self.collections_file.truncate(0)
         yaml.dump(
-            self.collections,
+            self._collections,
             self.collections_file,
             sort_keys=False,
-            encoding=None,
             allow_unicode=True,
         )
 
@@ -49,15 +67,15 @@ class CollectionsController:
         collection_name : str
             The name of the new collection
         """
-        while (
-            rnd_id := "".join(
+        while True:
+            rnd_id: str = "".join(
                 random.choices(string.ascii_lowercase + string.digits, k=16)
             )
-        ) not in os.listdir(self.collections_dir):
-            os.mkdir(self.collections_dir + f"/{rnd_id}/")
-            break
-        collection = {"name": collection_name, "id": rnd_id}
-        self.collections.append(collection)
+            if rnd_id not in os.listdir(self.collections_dir):
+                os.mkdir(os.path.join(self.collections_dir, rnd_id))
+                break
+        collection: dict = {"name": collection_name, "id": rnd_id}
+        self._collections.append(collection)
         self.save_collections()
 
     def remove_collection(self, collection_id: str) -> None:
@@ -70,30 +88,31 @@ class CollectionsController:
             id of the collection to be deleted
         """
         self.save_collections()
-        for collection in self.collections:
-            if collection["id"] == collection_id:
-                self.collections.remove(collection)
-                return
+        self._collections = [c for c in self._collections if c["id"] != collection_id]
 
     def undo_remove_collection(self) -> None:
         """Undoing remove action"""
-        self.collections: list = yaml.safe_load(self.collections_file)
+        self._collections = yaml.safe_load(self.collections_file) or []
 
     def remove_garbage(self) -> None:
         """Removes directories of the deleted collections
 
         Should be called on app close to provide 'Undo' action work
         """
-        collections_dirs = [
+        collections_dirs: list = [
             d
             for d in os.listdir(self.collections_dir)
             if os.path.isdir(os.path.join(self.collections_dir, d))
         ]
-        ids = [item.get("id") for item in self.collections]
+        ids: list = [item.get("id") for item in self._collections]
 
         for directory in collections_dirs:
             if directory not in ids:
-                shutil.rmtree(self.collections_dir + f"/{directory}")
+                shutil.rmtree(os.path.join(self.collections_dir, directory))
+
+    @GObject.Property
+    def collections(self) -> list:
+        return self._collections
 
 
 class CollectionController:
@@ -101,33 +120,32 @@ class CollectionController:
 
     Parameters
     ----------
-    collection_dir : str
+    collection_path : str
         /path/to/collection/directory
     """
 
     __gtype_name__ = "CollectionController"
 
     def __init__(self, collection_path: str) -> None:
-        if not os.path.exists(collection_path + "/collection.yaml"):
-            file = open(collection_path + "/collection.yaml", "x+", encoding="utf-8")
-            file.write("[]")
-            file.close()
+        self.collection_file_path: str = os.path.join(
+            collection_path, "collection.yaml"
+        )
+
+        if not os.path.exists(self.collection_file_path):
+            with open(self.collection_file_path, "w", encoding="utf-8") as file:
+                file.write("[]")
 
         self.collection_file: TextIO = open(
-            collection_path + "/collection.yaml", "r+", encoding="utf-8"
+            self.collection_file_path, "r+", encoding="utf-8"
         )
-        self._collection: list = yaml.safe_load(self.collection_file)
+        self._collection: list = yaml.safe_load(self.collection_file) or []
 
     def save_collection(self) -> None:
-        """Saves instace collection to the file"""
+        """Saves instance collection to the file"""
         self.collection_file.seek(0)
         self.collection_file.truncate(0)
         yaml.dump(
-            self._collection,
-            self.collection_file,
-            sort_keys=False,
-            encoding=None,
-            allow_unicode=True,
+            self._collection, self.collection_file, sort_keys=False, allow_unicode=True
         )
 
     def add_lexicon(self, lexicon_name: str) -> None:
@@ -138,17 +156,15 @@ class CollectionController:
         lexicon_name : str
             The name of the new lexicon
         """
-        if lexicon_name not in os.listdir(os.path.dirname(self.collection_file.name)):
-            os.makedirs(
-                os.path.dirname(self.collection_file.name)
-                + f"/{lexicon_name}/resources"
-            )
-            open(
-                os.path.dirname(self.collection_file.name)
-                + f"/{lexicon_name}/lexicon.yaml",
-                "w",
-                encoding="utf-8",
-            ).write("[]")
+        lexicon_path: str = os.path.join(
+            os.path.dirname(self.collection_file.name), lexicon_name
+        )
+        if not os.path.exists(lexicon_path):
+            os.makedirs(os.path.join(lexicon_path, "resources"))
+            with open(
+                os.path.join(lexicon_path, "lexicon.yaml"), "w", encoding="utf-8"
+            ) as file:
+                file.write("[]")
             self._collection.append({"name": lexicon_name, "tags": []})
             self.save_collection()
         else:
@@ -158,7 +174,7 @@ class CollectionController:
 
     def remove_lexicon(self, lexicon_name: str) -> None:
         """Removes lexicon by provided `name`.\
-           Saves instace collection to the file first to allow 'Undo' action
+            Saves instance collection to the file first to allow 'Undo' action
 
         Parameters
         ----------
@@ -166,33 +182,30 @@ class CollectionController:
             id of the lexicon to be deleted
         """
         self.save_collection()
-        for lexicon in self._collection:
-            if lexicon["name"] == lexicon_name:
-                self._collection.remove(lexicon)
-                return
+        self._collection = [l for l in self._collection if l["name"] != lexicon_name]
 
     def undo_remove_lexicon(self) -> None:
         """Undoing remove action"""
-        self._collection: list = yaml.safe_load(self.collection_file)
+        self._collection = yaml.safe_load(self.collection_file) or []
 
     def remove_garbage(self) -> None:
         """Removes directories of the deleted lexicon
 
         Called on demand, but 'Undo' action is unavailable
         """
-        lexicons = [
+        lexicons: list = [
             d
             for d in os.listdir(os.path.dirname(self.collection_file.name))
             if os.path.isdir(
                 os.path.join(os.path.dirname(self.collection_file.name), d)
             )
         ]
-        names = [item.get("name") for item in self._collection]
+        names: list = [item.get("name") for item in self._collection]
 
         for lexicon in lexicons:
-            if lexicons not in names:
+            if lexicon not in names:
                 shutil.rmtree(
-                    os.path.dirname(self.collection_file.name) + f"/{lexicon}"
+                    os.path.join(os.path.dirname(self.collection_file.name), lexicon)
                 )
 
     @GObject.Property
@@ -206,28 +219,21 @@ class LexiconController:
     Parameters
     ----------
     lexicon_path : str
-        /path/to/lexicon/file
+        /path/to/lexicon/directory
     """
 
     __gtype_name__ = "LexiconController"
 
     def __init__(self, lexicon_path: str) -> None:
-        self.lexicon_path: str = lexicon_path
-        self.resources: str = os.path.dirname(lexicon_path) + "/resources"
-        self.lexicon_file: TextIO = open(lexicon_path, "r+", encoding="utf-8")
+        self.resources: str = os.path.join(lexicon_path, "resources")
+        self.lexicon_file: TextIO = open(os.path.join(lexicon_path, "lexicon.yaml"))
         self._lexicon: list = yaml.safe_load(self.lexicon_file)
 
     def save_lexicon(self) -> None:
         """Saves instace lexicon to the file"""
         self.lexicon_file.seek(0)
         self.lexicon_file.truncate(0)
-        yaml.dump(
-            self._lexicon,
-            self.lexicon_file,
-            sort_keys=False,
-            encoding=None,
-            allow_unicode=True,
-        )
+        yaml.dump(self._lexicon, self.lexicon_file, sort_keys=False, allow_unicode=True)
 
     def add_pair(self, key: str) -> None:
         """Adds new pair to the instance lexicon and saves it to the file
@@ -237,15 +243,13 @@ class LexiconController:
         key : str
             The key of the new pair
         """
-        while (
-            rnd_id := "".join(
+        while True:
+            rnd_id: str = "".join(
                 random.choices(string.ascii_lowercase + string.digits, k=16)
             )
-        ) not in os.listdir(self.resources):
-            os.mkdir(
-                self.resources + f"/{rnd_id}",
-            )
-            break
+            if rnd_id not in os.listdir(self.resources):
+                os.mkdir(os.path.join(self.resources, rnd_id))
+                break
 
         self._lexicon.append(
             {"key": key, "value": None, "resources_id": rnd_id, "resources": []}
@@ -258,37 +262,32 @@ class LexiconController:
 
         Parameters
         ----------
-        resource_id : str
+        resources_id : str
             id of the pair resources to be deleted
         """
         self.save_lexicon()
-        for pair in self._lexicon:
-            if pair["resources_id"] == resources_id:
-                self._lexicon.remove(pair)
-                return
+        self._lexicon = [p for p in self._lexicon if p["resources_id"] != resources_id]
 
     def undo_remove_pair(self) -> None:
         """Undoing remove action"""
-        self._lexicon = yaml.safe_load(self.lexicon_file)
+        self._lexicon = yaml.safe_load(self.lexicon_file) or []
 
     def remove_garbage(self) -> None:
         """Removes resources of the deleted pair
 
         Called on demand, but 'Undo' action is unavailable
         """
-        resources = [
+        resources: list = [
             d
-            for d in os.listdir(
-                os.path.join(os.path.dirname(self.lexicon_path), "resources")
-            )
-            if os.path.isdir(
-                os.path.join(os.path.dirname(self.lexicon_path), "resources", d)
-            )
+            for d in os.listdir(self.resources)
+            if os.path.isdir(os.path.join(self.resources, d))
         ]
-        res_ids = [item.get("resources_id") for item in self._lexicon]
+        res_ids: list = [item.get("resources_id") for item in self._lexicon]
 
         for resource in resources:
             if resource not in res_ids:
-                shutil.rmtree(
-                    os.path.dirname(self.lexicon_path) + f"/resources/{resource}"
-                )
+                shutil.rmtree(os.path.join(self.resources, resource))
+
+    @GObject.Property
+    def lexicon(self) -> list:
+        return self._lexicon
